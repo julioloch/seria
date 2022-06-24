52f49f2f21665c8ec38ede7dab2a17d4285704ec
Julio Loch <julioloch@hotmail.com>
2022-06-24 03:42:27 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let seria =['Agua aquática','Terra terrestre','Roda redonda','Quadro quadrado','Espelho espelhado','Placa emplacada','Molho molhado','Foco focado'];
+alert("seria? (clica ok)")
+
+for(let i=0; i<=seria.length-1; i++){
+	alert(seria[i])
+}
